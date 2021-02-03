b80bc61f1ac59a7842e09d38b1f3505339db7028
Torben Brodt <torben@opinary.com>
2021-02-03 03:49:58 +0100

    Add Opinary Bid Adapter

---
+++ modules/opinaryBidAdapter.js
@@ -0,0 +1,92 @@
+import { deepAccess, parseSizesInput } from 'src/utils.js';
+import {registerBidder} from '../src/adapters/bidderFactory.js';
+
+export const spec = {
+  code: 'opinary',
+  /**
+   * @param {object} bid
+   * @return boolean
+   */
+  isBidRequestValid: function (bid) {
+    if (!deepAccess(bid, 'params.customer')) {
+      return false;
+    }
+    let sizes = parseSizesInput(bid.sizes);
+    // currently opinary only supports 500x500
+    if (sizes.filter(size => size == "500x500").length === 0) {
+      return false;
+    }
+    return true;
+  },
+  /**
+   * @param {BidRequest[]} bidRequests
+   * @param bidderRequest
+   * @return ServerRequest[]
+   */
+  buildRequests: (validBidRequests, bidderRequest) => {
+    let validBidRequest = validBidRequests.filter(x => x.bidderRequestId === bidderRequest.bidderRequestId);
+    if (validBidRequest.length === 0) {
+      return {};
+    }
+
+    let payload = {};
+    payload.customer = validBidRequest[0].params.customer;
+    payload.url = bidderRequest.refererInfo.canonicalUrl || bidderRequest.refererInfo.referer;
+    payload.requestId = validBidRequest[0].bidId;
+    payload.amp = bidderRequest.refererInfo.isAmp;
+    payload.adblocker = false;
+    if (deepAccess(bidderRequest, 'gdprConsent.gdprApplies')) {
+      payload.gdpr = bidderRequest.gdprConsent.gdprApplies;
+      payload.gdpr_consent = bidderRequest.gdprConsent.consentString;
+    }
+
+    return {
+      method: 'GET',
+      url: 'https://api.opinary.com/v1/ams/recommendations',
+      data: payload
+    };
+  },
+  /**
+   * @param {*} responseObj
+   * @param {BidRequest} bidRequest
+   * @return {Bid[]} An array of bids which
+   */
+  interpretResponse: function (serverResponse, bidRequest) {
+    const body = serverResponse.body;
+
+    // check overall response
+    if (body == null || typeof body !== 'object' || !body.hasOwnProperty('pollURL')) {
+      return [];
+    }
+
+    // TODO: this is what we expect from the API
+    let fakeResponse = {
+      requestId: bidRequest.data.requestId,
+      width: 500,
+      height: 500,
+      creativeId: 1, // TODO: INTEGER,buyer_creative_id field (64 byte limit)
+      cpm: 4.81, // TODO: relevancy based score, see _meta.result.score(float)
+      currency: 'USD', // TODO
+      isNetRevenue: true, // TODO
+      dealId: 0, // TODO
+      ttl: 360 // TODO
+    };
+
+    let adm = `<iframe scrolling="no" frameborder="0" width="${fakeResponse.width}" height="${fakeResponse.height}" src="${body.pollURL}"></iframe>`;
+    let bid = {
+      requestId: fakeResponse.requestId,
+      cpm: fakeResponse.cpm,
+      width: fakeResponse.width,
+      height: fakeResponse.height,
+      size: fakeResponse.width + "x" + fakeResponse.height,
+      ad: adm,
+      ttl: fakeResponse.ttl,
+      creativeId: fakeResponse.creativeId, 
+      netRevenue: fakeResponse.isNetRevenue,
+      currency: fakeResponse.currency,
+      dealId: fakeResponse.dealId
+    };
+    return [bid];
+  }
+};
+registerBidder(spec);
---
+++ modules/opinaryBidAdapter.md
@@ -0,0 +1,35 @@
+# Overview
+
+```
+Module Name:  Opinary Bid Adapter
+Module Type:  Bidder Adapter
+Maintainer: prebid@opinary.com
+```
+
+# Description
+
+Connects to Opinary for direct sponsored polls.
+
+Opinary bid adapter supports Banner.
+
+Currently only 500x500 is supported.
+
+# Test Parameters
+```
+var adUnits = [
+   {
+      code: 'banner-div',
+      mediaTypes: {
+        banner: {
+          sizes: [[500, 500]]
+        }
+      },
+      bids: [{
+         bidder: 'opinary',
+         params: {
+           customer: 'focus'
+         }
+       }]
+   }
+];
+```
